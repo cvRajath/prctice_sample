e00b5505dbd0da3081cffa3155054889caa9c420
cvRajath <rajathcv0001@gmail.com>
2024-06-29 19:27:56 +0530

    commit

---
+++ 1.js
@@ -0,0 +1,134 @@
+
+
+let hi = "ghgh" ;
+let hlo = "fgfg";
+let ht = "gr" ;
+
+console.log(ht)
+                                 
+///////////////////////////////////////////////////////
+let num1 = 50;
+let num2 = 50;
+ let result = num1+num2;
+
+console.log(`the sum of 2 num is ${result}`)
+///////////////////////////////////////////////////////////
+
+let num1 = 30;
+let num2 = 30;
+let result = num1+ num2
+console.log(`the sum of 2 numbers is = ${result}`)
+
+/////////////////////////////////////////////////////////
+
+let value = 222;
+let num = 200;
+let answer = value+ num;
+console.log(`the answer is : ${answer}`)
+///////////////////////////////////////////////////////////////
+
+let nums = 20;
+let num1 = 20;
+let sum = nums+num1;
+
+let num = document.querySelector(".num");
+num.innerHTML = `the sum of 2 numbers is : ${sum}`;
+
+console.log(`the sum of 2 numbers is ${sum}`)          
+
+////////////////////////////////////////////////////////////////
+let num1 = "rajath";
+
+let namee = document.querySelector(".namee");
+namee.innerHTML = ` ${num1}`
+console.log(``)
+
+///////////////////////////////////////////////////////////////////
+let h = "rajath";
+let tt = document.querySelector('.tt');
+tt.innerHTML = ` ${h}`
+console.log(``)
+
+//////////////////////////////////////////////////////////////////
+ 
+let v = "hlo";
+
+let hi =document.querySelector(".hi");
+
+hi.innerHTML= `${v}`
+
+console.log(``)
+
+/////////////////////////////////////////////////////////////////
+let q = "re";
+
+let hi=document.querySelector(".hi");
+
+hi.innerHTML= `${q}`
+
+console.log()
+
+/////////////////////////////////////////////////////////////////
+ 
+let arr=["rt", "vb" ,"rtg","gf"];
+
+console.log(arr[3])
+//////////////////////////////////////////////////////////
+let arr=[21,3,34,54,40]
+arr[4]=340;
+console.log(arr[4])
+////////////////////////////////////////////////////////////////////
+let arr=[200,500,56,555,3333]
+arr[2]=30;
+console.log(arr[2])
+/////////////////////////////////////////////////////////////////
+
+let arr=['hlo','hii','ttt','kk']
+arr[3]='ddddd'
+console.log(arr[3])
+console.log(arr)
+/////////////////////////////////////////////////////////////
+let allLi = document.querySelectorAll('li');
+console.log(allLi[0]);
+
+//////////////////////////////////////////////////////////////////
+
+let fruits = ['orange ', 'apple','banana','coconut'];
+
+for(let fruit of fruits){
+    console.log(fruits[i]);
+}
+///////////////////////////////////////////////////////////////
+
+let value = [];
+value.push(5);
+value.push(7);
+console.log(value);
+/////////////////////////////////////////////////////////////////////////
+let values=[5,6,7,6];
+console.log(values[3])
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
